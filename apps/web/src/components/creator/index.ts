98b156fb1a3c1de4ec93fc7dc681c4997b72d10d
jamesmcarthur-3999 <jamesmcarthur3999@gmail.com>
2026-02-02 12:10:14 -0800

    feat: add creator component exports

---
+++ apps/web/src/components/creator/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Creator Components
+ *
+ * Components for the conversational BaleyBot creation experience.
+ */
+
+export { Canvas } from './Canvas';
+export { ChatInput } from './ChatInput';
+export { ActionBar } from './ActionBar';
